dc9d9bb01f21889d88d2e8fb5a092b79dcad45a8
Carlos Aranguren <arangurencg2@gmail.com>
2025-12-15 11:59:29 -0400

    procesar-recepcion, confirmacion-recepcion

--- notas_entrega/urls.py
+++ notas_entrega/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import render_notas_entrega, search_product_by_code, delete_product, search_product_by_description, modal_search, search_order, search_proveedor, procesar_recepcion
+from .views import render_notas_entrega, search_product_by_code, delete_product, search_product_by_description, modal_search, search_order, search_proveedor, procesar_recepcion, obtener_confirmacion
 urlpatterns = [
     path('notas-entrega/', render_notas_entrega, name='notas-entrega'),
     path('search-product/', search_product_by_code, name='search-product'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('search-order/', search_order, name='search-order'),
     path('product-delete/', delete_product, name='product-delete'),
     path('search-proveedor/', search_proveedor, name='search-proveedor'),
-    path('procesar-recepcion/', procesar_recepcion, name='procesar-recepcion')
+    path('procesar-recepcion/', procesar_recepcion, name='procesar-recepcion'),
+    path('confirmar-nota-entrega/', obtener_confirmacion, name='confirmar-nota-entrega')
 ]
